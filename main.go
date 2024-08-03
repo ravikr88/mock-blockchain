92bb8424f2b3eb4a98df5c193edb18ae21e9d581
ravikr-opnsrc <ravikr.opnsrc@gmail.com>
2024-08-03 18:45:22 +0530

    implement signatures for transactions

--- main.go
+++ main.go
@@ -14,9 +14,12 @@ func init() {
 func main() {
 
 	w := wallet.NewWallet()
+
 	// fmt.Println(w.PrivateKeyStr())
 	// fmt.Println(w.PublicKeyStr())
+	// fmt.Println(w.BlockchainAddress())
 
-	fmt.Println(w.BlockchainAddress())
+	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	fmt.Printf("signature %s \n", t.GenerateSignature())
 
 }
